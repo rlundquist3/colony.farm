d27670443064924de56322a76ccf44a8eec2782a
Riley Lundquist <rlundquist3@gmail.com>
2016-02-09 00:31:50 -0500

    added startup cost field

--- startup_cost/startup_cost.py
+++ startup_cost/startup_cost.py
@@ -31,22 +31,12 @@ def startup_cost():
 
     calculate()
 
-    del crickets[:]
-    crickets.append(data['start_crickets'])
-    data['cycles_to_break_even'] = time_to_break_even()
-    data['months_to_break_even'] = data['cycle_time'] * data['cycles_to_break_even']/4.
-
-    # df = pandas.DataFrame(
-    #     {'y':np.random.randn(24), 'z':np.random.randn(24)},
-    #     index=pandas.period_range('6/2016', periods=24, freq='M'),
-    # )
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    # df.plot(ax=ax)
     ax.plot(range(data['cycles_to_break_even']), crickets, '-b')
+    ax.set_title('Cricket population over time until break even')
     ax.set_xlabel('cycles (%s weeks)' % (data['cycle_time']))
     ax.set_ylabel('total crickets (none harvested)')
-
     io = StringIO()
     fig.savefig(io, format='png')
     img = base64.encodestring(io.getvalue())
@@ -57,12 +47,21 @@ def calculate():
     data['feed_cost_kg'] = data['kg_feed_price'] * data['feed_per_kg']
     data['kg_profit'] = data['kg_cricket_price'] - data['feed_cost_kg']
     data['break_even_kg'] = (data['rent'] + data['employees'] * data['wages']) / data['kg_profit']
-    data['break_even_crickets'] = data['break_even_kg'] * data['crickets_per_kg']
+    data['break_even_crickets']  = data['break_even_kg'] * data['crickets_per_kg']
+    data['reserve_crickets'] = data['break_even_crickets'] / (data['eggs_per_female']/2 * data['hatch_rate'] - 1)
+    data['cohort_size'] = data['break_even_crickets'] + data['reserve_crickets']
+
+    del crickets[:]
+    crickets.append(data['start_crickets'])
+    data['cycles_to_break_even'] = time_to_break_even()
+    data['months_to_break_even'] = data['cycle_time'] * data['cycles_to_break_even']/4.
+
+    data['seed'] = data['months_to_break_even'] * (data['wages'] * data['employees'] + data['rent'] + data['start_crickets'] * 0.02)
 
 def time_to_break_even():
     new_crickets = crickets[-1]/2 * data['eggs_per_female'] * data['hatch_rate']
     crickets.append(new_crickets)
-    if new_crickets >= data['break_even_crickets']:
+    if new_crickets >= data['cohort_size']:
         return len(crickets)
     else:
         return time_to_break_even()
