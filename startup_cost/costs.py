34ea285e1778d0b387c1d0c6acd4b5a7e4c30677
Riley Lundquist <rlundquist3@gmail.com>
2016-02-14 17:39:14 -0500

    switched to jade

--- startup_cost/costs.py
+++ startup_cost/costs.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python2
-'''Serving dynamic images with Pandas and matplotlib (using flask).'''
 
 import matplotlib
 matplotlib.use('Agg')
@@ -15,12 +14,15 @@ from flask import render_template
 from flask import request
 app = Flask(__name__)
 
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
 data = {'rent' : 15000, 'kg_cricket_price' : 80,
         'kg_feed_price' : 2, 'feed_per_kg' : 1.7,
         'crickets_per_kg' : 10000, 'eggs_per_female' : 200,
         'hatch_rate' : 0.5, 'start_crickets' : 5000,
         'cycle_time' : 11, 'wages' : 3000,
-        'employees' : 3, 'scaling_factor' : 2}
+        'employees' : 3, 'scaling_factor' : 2,
+        'months_before_rent' : 3}
 crickets = []
 
 @app.route('/startup_cost', methods=['POST', 'GET'])
@@ -49,7 +51,7 @@ def startup_cost():
     fig.savefig(io, format='png')
     img = base64.encodestring(io.getvalue())
 
-    return render_template('startup_cost.html', graph = img, data = data)
+    return render_template('costs.jade', graph = img, data = data)
 
 def calculate():
     data['feed_cost_kg'] = data['kg_feed_price'] * data['feed_per_kg']
@@ -64,8 +66,8 @@ def calculate():
     crickets.append(data['start_crickets'])
     data['cycles_to_break_even'] = time_to_break_even()
     data['months_to_break_even'] = data['cycle_time'] * data['cycles_to_break_even']/4.
-
-    data['seed'] = data['months_to_break_even'] * (data['wages'] * data['employees'] + data['rent'] + data['start_crickets'] * 0.02)
+    data['seed'] = (data['months_to_break_even'] - data['months_before_rent']) * (data['wages'] * data['employees'] + data['rent'])
+    # cost of housing
 
 def time_to_break_even():
     new_crickets = crickets[-1]/2 * data['eggs_per_female'] * data['hatch_rate']
